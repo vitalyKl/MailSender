937b52e25e220e765afd07f128c01342c5cb53d7
Vitaly Kalinin <vital5991@gmail.com>
2021-02-12 21:39:53 +0300

    Добавьте файлы проекта.

---
+++ TestConsole/TestConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var from = new MailAddress("vital5991@yandex.ru", "Vitali");
+            var to = new MailAddress("vital5991@yandex.ru", "Vitali");
+            var message = new MailMessage(from, to);
+            message.Body = "YO!";
+            message.Subject = "YO!";
+
+
+            var client = new SmtpClient("smpt.yandex.ru", 465);
+            client.EnableSsl = true;
+
+            client.Credentials = new NetworkCredential
+            {
+                UserName = "mstester27102020@yandex.ru",
+                Password = "mstester27102020"
+            };
+
+            client.Send(message);
+        }
+    }
+}
---
+++ TestWPF/TestWPF/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var from = new MailAddress("mstester27102020@yandex.ru", "Vitali");
+            var to = new MailAddress("vital5991@yandex.ru", "Vitali");
+            var message = new MailMessage(from, to);
+            message.Body = "YO!";
+            message.Subject = "YO!";
+
+
+            var client = new SmtpClient("smtp.yandex.ru", 25);
+            client.EnableSsl = true;
+
+            client.Credentials = new NetworkCredential
+            {
+                UserName = Login.Text,
+                SecurePassword = Password.SecurePassword
+            };
+
+            client.Send(message);
+        }
+    }
+}
