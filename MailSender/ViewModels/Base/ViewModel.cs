671f6624aab0af8312e74783143426769ca13882
Vitaly Kalinin <vital5991@gmail.com>
2021-02-26 16:18:42 +0300

    Создан базовый класс ViewModel и ViewModel для MessagesWindow

---
+++ MailSender/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.ViewModels.Base
+{
+    abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
---
+++ MailSender/ViewModels/MessagesWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using MailSender.View;
+using MailSender.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.ViewModels
+{
+    class MessagesWindowViewModel: ViewModel
+    {
+        private ObservableCollection<Message> _Messages;
+
+        public ObservableCollection<Message> Messages
+        {
+            get => _Messages;
+            set => Set(ref _Messages, value);
+        }
+    }
+}
